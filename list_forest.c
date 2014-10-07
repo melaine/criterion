852175d0c35ea26692aa081c66588c40bb1b34bf
Valball <val_jub@live.fr>
2014-10-07 08:18:27 +0200

    correction erreur dans list_forest.c

--- list_forest.c
+++ list_forest.c
@@ -4,7 +4,7 @@
  *  Created on: 6 oct. 2014
  *      Author: Valentin Jubert/ Melaine Gbahi
  */
-
+#include <stdlib.h>
 #include "list_forest.h"
 
 typedef struct list_forest_struct {
@@ -13,17 +13,21 @@ typedef struct list_forest_struct {
 	list_forest* father;
 	list_forest* brother;
 	list_forest* son;
-} list_forest;
+} * list_forest;
 
-typedef struct list_forest_position_struct {
+typedef struct list_forest_position_struct{
 	list_forest * here;
-} list_forest_position ;
+} * list_forest_position ;
 
-extern int list_forest_position_create ( list_forest li, list_forest_position *  pos) {
-	pos->here=malloc(sizeof(list_forest));
-	pos.here=&li;
-
+extern int list_forest_position_create ( list_forest li, list_forest_position * pos) {
+	pos=malloc(sizeof(list_forest_position));
+	(*pos)->here=malloc(sizeof(list_forest));
+	(*pos)->here=&li;
+	return FOREST_LIST_OK;
 }
-extern int list_forest_is_empty ( list_forest li) {
+extern int list_forest_position_destroy ( list_forest_position * pos){
+	free((*pos)->here);
+	free(*pos);
+	return FOREST_LIST_OK;
 
 }
